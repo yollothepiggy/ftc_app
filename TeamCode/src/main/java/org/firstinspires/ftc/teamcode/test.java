4abd3160abfa55e8f4a6639db3bd2e44bd3d22da
ADSERV\mendezis <yollothepiggy@gmail.com>
2018-11-16 08:32:26 -0800

    New test class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test.java
@@ -1,8 +1,48 @@
 package org.firstinspires.ftc.teamcode;
 
-/**
- * Created by mendezis on 11/16/2018.
- */
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.Range;
+import com.qualcomm.robotcore.hardware.Servo;
 
-public class test {
+
+@TeleOp(name="OFFICIAL TeleOp", group="Exercises")
+
+//@Disabled
+
+public class test extends LinearOpMode
+
+{
+    DcMotor leftMotor, rightMotor;
+    double lefts, rights;
+
+    @Override
+    public void runOpMode() {
+        //First Hub
+        leftMotor = hardwareMap.dcMotor.get("leftMotor");
+        rightMotor = hardwareMap.dcMotor.get("rightMotor");
+        leftMotor.setDirection(DcMotor.Direction.REVERSE);
+
+
+        waitForStart();
+
+        while (opModeIsActive())
+
+        {
+
+            // Gamepad 1 controls
+
+            lefts = gamepad1.left_stick_y;
+            rights = gamepad1.right_stick_y;
+
+
+
+            leftMotor.setPower(Range.clip(lefts, -.50, .75));
+            rightMotor.setPower(Range.clip(rights, -.50, .75));
+
+
+
+        }
+    }
 }
