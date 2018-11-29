c2cea3d06420c5246d0ce643c14b14d06a6b6867
ADSERV\mendezis <yollothepiggy@gmail.com>
2018-11-29 07:54:52 -0800

    AutoCrater

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoCrater.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.Range;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@Autonomous(name="OFFICIAL TeleOp", group="Exercises")
+/**
+ * Created by mendezis on 11/28/2018.
+ */
+
+public class AutoCrater extends LinearOpMode {
+
+    public void runOpMode()
+    {
+
+    }
+}
