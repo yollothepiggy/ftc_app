720d2494bc7f448d49ea736f90482ed77b7c1be4
ADSERV\mendezis <yollothepiggy@gmail.com>
2019-09-10 10:19:20 -0700

    Last Year stuff
    random stuff that i have done

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Racer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Racer.java
@@ -5,14 +5,14 @@ package org.firstinspires.ftc.teamcode;
         import com.qualcomm.robotcore.hardware.DcMotor;
         import com.qualcomm.robotcore.util.Range;
 
-@TeleOp(name="Drag", group="Exercises")
+@TeleOp(name="Sumo", group="Exercises")
 
 //@Disabled
 
 public class Racer extends LinearOpMode
 
 {
-    DcMotor leftMotor, rightMotor;
+    DcMotor leftMotor, rightMotor,lift, lift2;
     double lefts, rights;
 
     @Override
@@ -20,7 +20,9 @@ public class Racer extends LinearOpMode
         //First Hub
         leftMotor = hardwareMap.dcMotor.get("leftMotor");
         rightMotor = hardwareMap.dcMotor.get("rightMotor");
-        leftMotor.setDirection(DcMotor.Direction.REVERSE);
+        rightMotor.setDirection(DcMotor.Direction.REVERSE);
+        lift = hardwareMap.dcMotor.get("lift");
+        lift2 = hardwareMap.dcMotor.get("lift2");
 
         telemetry.addData("mode", "waiting");
         telemetry.update();
@@ -37,12 +39,32 @@ public class Racer extends LinearOpMode
             lefts = gamepad1.left_stick_y;
             rights = gamepad1.right_stick_y;
 
-            leftMotor.setPower(-Range.clip(-lefts, -.50, 1));
-            rightMotor.setPower(-Range.clip(-rights, -.50, 1));
+            leftMotor.setPower(-Range.clip(-lefts, -1, 1));
+            rightMotor.setPower(-Range.clip(-rights, -1, 1));
 
             telemetry.addData("Left Motor", leftMotor.getPower());
             telemetry.addData("Right Motor", rightMotor.getPower());
             telemetry.update();
+
+
+            if (gamepad1.dpad_up)
+            {
+                lift.setPower(1);
+                lift2.setPower(-1);
+            }
+
+            if (gamepad1.b)
+            {
+                lift.setPower(0);
+                lift2.setPower(0);
+            }
+
+            if (gamepad1.dpad_down)
+            {
+                lift.setPower(-1);
+                lift2.setPower(1);
+            }
+
 
         }
     }
